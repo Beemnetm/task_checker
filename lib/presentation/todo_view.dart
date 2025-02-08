a8ef9cbda635fe9bc8b0a9f0df06235d3224cdd5
Beemnetm <beemnetmulugeta@gmail.com>
2025-02-08 20:15:15 +0300

    functionality

---
+++ lib/presentation/todo_view.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_checker/domain/models/todo.dart';
+import 'package:task_checker/presentation/todo_cubit.dart';
+
+class TodoView extends StatefulWidget {
+  const TodoView({super.key});
+
+  @override
+  State<TodoView> createState() => _TodoViewState();
+}
+
+class _TodoViewState extends State<TodoView> {
+  void _showAddTodoBox(BuildContext context) {
+    final todoCubit = context.read<TodoCubit>();
+    final textController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: () {
+              if (textController.text.trim().isNotEmpty) {
+                todoCubit.addTodo(textController.text.trim());
+                Navigator.of(context).pop();
+              }
+            },
+            child: const Text("Add"),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final todoCubit = context.read<TodoCubit>();
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _showAddTodoBox(context),
+      ),
+      body: BlocBuilder<TodoCubit, List<Todo>>(
+        builder: (context, todos) {
+          return ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (context, index) {
+              final todo = todos[index];
+
+              return ListTile(
+                title: Text(todo.text),
+                leading: Checkbox(
+                  value: todo.isCompleted,
+                  onChanged: (value) => todoCubit.toggleCompletion(todo),
+                ),
+                trailing: IconButton(
+                  onPressed: () => todoCubit.deleteTodo(todo),
+                  icon: const Icon(Icons.cancel),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
